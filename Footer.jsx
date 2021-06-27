8b7cb9ae922716e1564b82305524ae537f0aa6d2
Aromalp2323 <86562944+Aromalp2323@users.noreply.github.com>
2021-06-28 03:59:16 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer() {
+
+  return (
+   <footer>
+       <p>Copyright by SHAPEAI @ {new Date().getFullYear()} </p>
+            </footer>
+                
+                    );
+                    }
+
+                    export default Footer;
---
+++ Info.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>Java script and React.js</h1>
+      <p>
+        7 Days, Certified, Free Javascript and React.js(15th June to 21th June)
+        📢Here we bring our another free live BOOTCAMP with a hands on project
+        at the end; Basic Front end Development with Javascript and React.js-
+        The Foundation of Web Development 🖥 Prerequisite : None Duration- 7 days
+        (15th June onwards) Topics to be covered- • INTRO TO PROGRAMMING IN
+        JAVASCRIPT • INTRODUCTION TO HTML • FOUNDATIONAL KNOWLEDGE IN REACT.JS •
+        HANDS ON PROJECT And much more... So what are you waiting for? Learn
+        from upcoming intern at JIO, Microsoft and Goldman Sachs. And get
+        CERTIFIED✨ at the end. Hurry! Register now:
+        https://forms.gle/HuT5cLHtmYDxqvvZ9 For more Bootcamps visit :
+        https://www.shapeai.tech/eventsii
+      </p>
+    </div>
+  );
+}
+export default Info;
